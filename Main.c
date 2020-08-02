bcf267ba0a6465c76077204ccb11157af74052b2
Administrator <10567361@polimi.it>
2020-08-02 11:25:14 +0200

    Implemented GUI interface

--- Main.c
+++ Main.c
@@ -1,87 +1,86 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-#include<malloc.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <gtk/gtk.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
 #ifdef _WIN32
 #include <windows.h>
 #endif
 #ifdef linux
 #include <unistd.h>
 #endif
-#include<math.h>
 
+#define row 16
+#define col 30
 #define ALIVE 1
 #define DEAD 0
-#define SIZE_X 30
-#define SIZE_Y 90
 //the update delay in ms
-#define DELAY 1000
+#define DELAY 200
 
 typedef struct {
-    char **oldMap;
-    char **newMap;
-    int size_x;
-    int size_y;
+    GtkWidget *grid;
+    bool **oldMap;
+    bool **newMap;
+    int columns;
+    int rows;
 } maps_t;
+
+void instanciateMap(maps_t *map) {
+    map->oldMap = malloc(map->columns * sizeof(bool **));
+    map->newMap = malloc(map->columns * sizeof(bool **));
+    for (int i = 0; i < map->columns; i++) {
+        map->oldMap[i] = malloc(map->rows * sizeof(bool *));
+        map->newMap[i] = malloc(map->rows * sizeof(bool *));
+    }
+}
 
 /**
  * creates the [SIZE_X] x [SIZE_Y] map
 */
 void createRandomMap(maps_t *maps) {
-    maps -> oldMap = malloc(sizeof(char**));
-    maps -> newMap = malloc(sizeof(char**));
-    maps -> oldMap = malloc(SIZE_X * sizeof(char**));
-    maps -> newMap = malloc(SIZE_X * sizeof(char**));
-    maps -> size_x = SIZE_X;
-    maps -> size_y = SIZE_Y;
-    for(int i = 0; i < SIZE_X; i++) {
-        maps -> oldMap[i] = malloc(SIZE_Y * sizeof(char*));
-        maps -> newMap[i] = malloc(SIZE_Y * sizeof(char*));
-        for(int j = 0; j < SIZE_Y; j++) {
-            int random = rand() % 2;
-            if(random == 0) {
-                maps -> oldMap[i][j] = DEAD;
-            } else {
-                maps -> oldMap[i][j] = ALIVE;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (rand() % 2 == 0) {
+                maps->oldMap[i][j] = false;
+            }
+            else {
+                maps->oldMap[i][j] = true;
+                gtk_widget_set_name(GTK_WIDGET(gtk_grid_get_child_at(GTK_GRID(maps->grid), j, i)), "black");
             }
         }
     }
 }
 
-/**
- * deserialize an existing map, formatted as
- * row column
- * 011100101110100
- * 101010110101101
- * ...
-*/
 void deserializeMap(maps_t *maps, char *fileName) {
     FILE *file = fopen(fileName, "r");
     int sizeX, sizeY;
     char alive;
-    if(file == NULL) {
+    if (file == NULL) {
         printf("An error occurred while opening the file\n");
         exit(0);
     }
-    fscanf(file, "%d", &sizeX);
-    fseek(file, 1, SEEK_CUR);
     fscanf(file, "%d", &sizeY);
-    maps -> oldMap = malloc(sizeof(char**));
-    maps -> newMap = malloc(sizeof(char**));
-    maps -> oldMap = malloc(sizeX * sizeof(char**));
-    maps -> newMap = malloc(sizeX * sizeof(char**));
-    maps -> size_x = sizeX;
-    maps -> size_y = sizeY;
+    fseek(file, 1, SEEK_CUR);
+    fscanf(file, "%d", &sizeX);
+    maps->oldMap = malloc(sizeof(bool **));
+    maps->newMap = malloc(sizeof(bool **));
+    maps->oldMap = malloc(sizeX * sizeof(bool **));
+    maps->newMap = malloc(sizeX * sizeof(bool **));
+    maps->columns = sizeX;
+    maps->rows = sizeY;
     fseek(file, 2, SEEK_CUR);
-    for(int i = 0; i < sizeX; i++) {
-        maps -> oldMap[i] = malloc(sizeY * sizeof(char*));
-        maps -> newMap[i] = malloc(sizeY * sizeof(char*));
-        for(int j = 0; j < sizeY; j++) {
+    for (int i = 0; i < sizeX; i++) {
+        maps->oldMap[i] = malloc(sizeY * sizeof(bool *));
+        maps->newMap[i] = malloc(sizeY * sizeof(bool *));
+        for (int j = 0; j < sizeY; j++) {
             fscanf(file, "%c", &alive);
-            if(alive - 48 == ALIVE) {
-                maps -> oldMap[i][j] = ALIVE;
-            } else {
-                maps -> oldMap[i][j] = DEAD;
+            if (alive - 48 == ALIVE) {
+                maps->oldMap[i][j] = ALIVE;
+            }
+            else {
+                maps->oldMap[i][j] = DEAD;
             }
         }
         fseek(file, 2, SEEK_CUR);
@@ -92,116 +91,133 @@ void deserializeMap(maps_t *maps, char *fileName) {
 /**
  * frees all the structures
 */
-void freeMap(maps_t *gameMaps) {
-    for(int i = 0; i < gameMaps -> size_x; i++) {
-        free(gameMaps -> oldMap[i]);
+void freeMap(GtkWidget *widget, gpointer data) {
+    maps_t *gameMaps = (maps_t*) data;
+    for(int i = 0; i < gameMaps -> columns; i++) {
         free(gameMaps -> newMap[i]);
+        free(gameMaps -> oldMap[i]);
     }
-    free(gameMaps -> oldMap);
     free(gameMaps -> newMap);
+    free(gameMaps -> oldMap);
     free(gameMaps);
+    gtk_main_quit();
+    exit(0);
 }
 
-/**
- * render the map
-*/
-void printMap(maps_t *gameMap) {
-    for(int i = 0; i < gameMap -> size_y + 2; i++) {
-        printf("-");
-    }
-    printf("\n");
-    for(int i = 0; i < gameMap -> size_x; i++) {
-        printf("|");
-        for(int j = 0; j < gameMap -> size_y; j++) {
-            if(gameMap -> oldMap[i][j] == 1) {
-                printf("#");
-            } else {
-                printf(" ");
-            }
-        }
-        printf("|\n");
-    }
-    for(int i = 0; i < gameMap -> size_y + 2; i++) {
-        printf("-");
-    }
-    printf("\n");
-}
-
-/**
- * counts the neighbors entities
-*/
-int countNear(maps_t *gameMap, int row, int column) {
+int countNear(maps_t *gameMap, int r, int c) {
     int num = 0;
-    int minRow = row - 1, minCol = column - 1, maxRow = row + 2, maxCol = column + 2;
-    if(minRow < 0 ) {
+    int minRow = r - 1, minCol = c - 1, maxRow = r + 2, maxCol = c + 2;
+    if (minRow < 0) {
         minRow = 0;
-    } else if(maxRow > gameMap -> size_x) {
-        maxRow = gameMap -> size_x;
     }
-    if(minCol < 0 ) {
+    else if (maxRow > gameMap->columns) {
+        maxRow = gameMap->columns;
+    }
+    if (minCol < 0) {
         minCol = 0;
-    } else if(maxCol > gameMap -> size_y) {
-        maxCol = gameMap -> size_y;
     }
-    for(int i = minRow; i < maxRow; i++) {
-        for(int j = minCol; j < maxCol; j++){
-            num += gameMap -> oldMap[i][j] == ALIVE;
+    else if (maxCol > gameMap->rows) {
+        maxCol = gameMap->rows;
+    }
+    for (int i = minRow; i < maxRow; i++) {
+        for (int j = minCol; j < maxCol; j++) {
+            num += gameMap->oldMap[i][j];
         }
     }
-    return num - gameMap -> oldMap[row][column];
+    return num - gameMap->oldMap[r][c];
 }
 
-/**
- * implements the game logic
-*/
-void game(maps_t *maps) {
+void myCSS(void) {
+    GtkCssProvider *provider;
+    GdkDisplay *display;
+    GdkScreen *screen;
+
+    provider = gtk_css_provider_new();
+    display = gdk_display_get_default();
+    screen = gdk_display_get_default_screen(display);
+    gtk_style_context_add_provider_for_screen(screen, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    const gchar *myCssFile = "style.css";
+    GError *error = 0;
+
+    gtk_css_provider_load_from_file(provider, g_file_new_for_path(myCssFile), &error);
+    g_object_unref(provider);
+}
+
+gint gameLogic(void *mapp) {
+    maps_t *map = (maps_t *)mapp;
     int num;
-    for(int i = 0; i < maps -> size_x; i++) {
-        for(int j = 0; j < maps -> size_y; j++) {
-            num = countNear(maps, i, j);
-            if(maps -> oldMap[i][j] == ALIVE) {
-                if(num < 2) {
-                    maps -> newMap[i][j] = DEAD;
-                } else if(num > 1) {
-                    maps -> newMap[i][j] = ALIVE;
+    boolean alive = false;
+    for (int i = 0; i < map->rows; i++) {
+        for (int j = 0; j < map->columns; j++) {
+            num = countNear(map, i, j);
+            if (map->oldMap[i][j]) {
+                if (num < 2) {
+                    map->newMap[i][j] = false;
                 }
-                if(num > 3) {
-                    maps -> newMap[i][j] = DEAD;
+                else if (num > 1) {
+                    map->newMap[i][j] = true;
                 }
-            } else {
-                if(num == 3) {
-                    maps -> newMap[i][j] = ALIVE;
-                } else {
-                    maps -> newMap[i][j] = DEAD;
+                if (num > 3) {
+                    map->newMap[i][j] = false;
                 }
+            }
+            else {
+                if (num == 3) {
+                    map->newMap[i][j] = true;
+                }
+                else {
+                    map->newMap[i][j] = false;
+                }
+            }
+            if (map->newMap[i][j]) {
+                alive = true;
+                gtk_widget_set_name(GTK_WIDGET(gtk_grid_get_child_at(GTK_GRID(map->grid), j, i)), "black");
+            }
+            else {
+                gtk_widget_set_name(GTK_WIDGET(gtk_grid_get_child_at(GTK_GRID(map->grid), j, i)), "white");
             }
         }
     }
-    char **temp = maps -> oldMap;
-    maps -> oldMap = maps -> newMap;
-    maps -> newMap = temp;
+    bool **temp = map->oldMap;
+    map->oldMap = map->newMap;
+    map->newMap = temp;
+    if (alive) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
 }
 
-int main() {
-    maps_t *maps = malloc(sizeof(maps_t));
-    char ris[100];
-    printf("Insert file name or \"random\": ");
-    scanf("%s", ris);
-    if(strcmp(ris, "random") == 0) {
-        createRandomMap(maps);
-    } else {
-        deserializeMap(maps, ris);
+int main(int argc, char *argv[]) {
+    GtkWidget *window, ***pos;
+    maps_t *map = malloc(sizeof(maps_t));
+
+    gtk_init(&argc, &argv);
+    myCSS();
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "The game of life");
+    g_signal_connect(window, "destroy", G_CALLBACK(freeMap), map);
+    map -> grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), map -> grid);
+
+    map -> columns = col;
+    map -> rows = row;
+    instanciateMap(map);
+    pos = malloc(map -> columns * sizeof(GtkWidget**));
+    for (int i = 0; i < map -> columns; i++) {
+        pos[i] = malloc(map -> rows * sizeof(GtkWidget*));
     }
-    while(1) {
-        printMap(maps);
-        game(maps);
-        #ifdef _WIN32
-        Sleep(DELAY);
-        #endif
-        #ifdef linux
-        sleep(DELAY / 1000);
-        #endif
+    for (int i = 0; i < map -> rows; i++) {
+        for (int j = 0; j < map -> columns; j++) {
+            pos[j][i] = gtk_button_new();
+            gtk_grid_attach(GTK_GRID(map -> grid), pos[j][i], j, i, 1, 1);
+        }
     }
-    freeMap(maps);
+    createRandomMap(map);
+    gtk_widget_show_all(window);
+    g_timeout_add(DELAY, gameLogic, map);
+    gtk_main();
     return 0;
 }
